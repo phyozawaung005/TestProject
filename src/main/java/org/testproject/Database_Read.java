8f707b05f1387fa3f6a75bfc7d5f7d5100c8a607
Phyo Zaw Aung <142676806+Ethansarfic12@users.noreply.github.com>
2025-05-08 14:24:13 +0630

    maven office display

--- src/main/java/org/testproject/Database_Read.java
+++ src/main/java/org/testproject/Database_Read.java
@@ -3,6 +3,7 @@ package org.testproject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Database_Read {
     private Connection con;
@@ -11,7 +12,9 @@ public class Database_Read {
         this.con = con;
     }
 
-    protected void read() {
+    protected ArrayList<office> read() {
+        ArrayList<office> aloffice = new ArrayList<>();
+
         try {
             //Preparedstatement can protect from SQL injection
             PreparedStatement ps = con.prepareStatement(
@@ -24,11 +27,16 @@ public class Database_Read {
             //next mean pointer, if run 1 time he will output data from first
             //if want to output next roll run 2 time. if you want to go up use previous
             //if data he pull data true/ if final data he told false.
+
+
+
             while(rs.next()){
-                System.out.print(rs.getInt(1));
-                System.out.print("|");
-                System.out.print(rs.getString("city"));
-                System.out.println("");
+                aloffice.add(new office(rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),rs.getString(4),
+                        rs.getString(5),rs.getString(6),
+                        rs.getString(7),rs.getString(8),
+                        rs.getString(9)));
             }
             rs.close();
             ps.close();
@@ -36,5 +44,13 @@ public class Database_Read {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    return aloffice;
     }
+
+    protected void display(ArrayList<office> aloffice) {
+
+    }
+
+
 }
--- src/main/java/org/testproject/Main.java
+++ src/main/java/org/testproject/Main.java
@@ -1,7 +1,5 @@
 package org.testproject;
 
-import jdk.internal.foreign.StringSupport;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
